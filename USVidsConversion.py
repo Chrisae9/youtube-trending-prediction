7ebdf151ca511d927a81e7ba4bb5be7745df17ff
chrisae9 <chrisae9@gmail.com>
2020-12-13 01:54:10 -0500

    Added code stuff

--- USVidsConversion.py
+++ USVidsConversion.py
@@ -1,77 +1,107 @@
 import pandas as pd
 import numpy as np
 import re
+from PIL import Image
+import requests
+import pytesseract
+import io
+from dateutil import parser
+from pandarallel import pandarallel
+
+pandarallel.initialize()
+
+def getLabel(publish, trending, views):
+    
+    # switch the year and the day
+    trending = trending.split(".")
+    trending[0], trending[2] = trending[2], trending[0]
+    trending = "-".join([x for x in trending])
+    trending = parser.parse(trending)
+
+    # cut off timezone
+    publish = publish.split('.')[0]
+    publish = parser.parse(publish)
+
+    time = trending - publish
+
+    if (time.days <= 1 and int(views) >= 1000000):
+        return 1
+    return 0
+
 
 def csv_conversion(file_name):
     df = pd.read_csv(file_name, encoding = "ISO-8859-1")
-    publish = df['publish_time']
-    trending = df['trending_date']
-    views = df['views']
-    titles = df['title']
-    row_count = df.shape[0]
-    a_million = 1000000
-    label_arr = np.array([], dtype = np.int)
-    clickbait_arr = np.array([], dtype = np.double)
+    df['label'] = df.parallel_apply(lambda x: getLabel(x['publish_time'], x['trending_date'], x['views']), axis=1)
+    
+    # 39234 rows
+    bad = df[df.label == 0]
+    # 1715 rows
+    good = df[df.label == 1]
 
-    for i in range(row_count):
-        pubNum = int(publish[i][8:10])
-        trendNum = int(trending[i][3:5])
-        viewsNum = int(views[i])
-        if ((pubNum == trendNum or pubNum + 1 == trendNum) and viewsNum >= a_million):
-            label_result = np.array([3], dtype = np.int)
-        elif ((pubNum == trendNum or pubNum + 1 == trendNum) and viewsNum < a_million):
-            label_result = np.array([2], dtype = np.int)
-        elif (not(pubNum == trendNum or pubNum + 1 == trendNum) and viewsNum >= a_million):
-            label_result = np.array([1], dtype = np.int)
-        else:
-            label_result = np.array([0], dtype = np.int)
-        clickbait_result = np.array(clickbait(titles[i]), dtype = np.double)    
-        clickbait_arr = np.append(clickbait_arr, clickbait_result)
-        label_arr = np.append(label_arr, label_result)
+    # shuffle and resample(underfit)
+    bad.sample(frac=1)
+    bad = df.iloc[:good.shape[0],:]
 
-    #np.savetxt('output.txt', label_arr, '%d', delimiter = ',')   
-    modified_df = df
-    modified_df['clickbait_score'] = clickbait_arr
-    modified_df['label'] = label_arr
-    return modified_df
+    # combine good and bad
+    df = pd.concat([good, bad])
+
+    df['clickbait_title_score'] = df.parallel_apply(lambda x: clickbaitTitle(x['title']), axis=1)
+    df['clickbait_image_score'] = df.parallel_apply(lambda x: clickbaitImage(x['thumbnail_link']), axis=1)
+
+    # print average clickbait score
+    averageClickbaitScore(df)
+
+    return df
 
 #clickbait # of capital letters in title / total amount of letters
-def clickbait(sentence):
-    return len(re.findall(r'[A-Z]', sentence)) / len(sentence)
+def clickbaitTitle(title):
+    return len(re.findall(r'[A-Z]', title)) / len(title)
 
-def averageClickbaitScore(dataframe):
-    labels = dataframe['label']
-    clickbait_scores = dataframe['clickbait_score']
-    row_count = dataframe.shape[0]
-    class0_arr = np.array([], dtype = np.double)
-    class1_arr = np.array([], dtype = np.double)
-    class2_arr = np.array([], dtype = np.double)
-    class3_arr = np.array([], dtype = np.double)
+def averageClickbaitScore(df):
+    bad = df[df.label == 0]
+    good = df[df.label == 1]
 
-    for i in range(row_count):
-        result = np.array(clickbait_scores[i])
-        if labels[i] == 0:
-            class0_arr = np.append(class0_arr, result)
-        elif labels[i] == 1:
-            class1_arr = np.append(class1_arr, result)
-        elif labels[i] == 2:
-            class2_arr = np.append(class2_arr, result)
-        else:
-            class3_arr = np.append(class3_arr, result)
+    print("Average good score:")
+    print(good['clickbait_title_score'].mean())
+
+    print("Average bad score:")
+    print(bad['clickbait_title_score'].mean())
+
+
+
+#url = "https://i.ytimg.com/vi/F5mzb086QM8/default.jpg"
+#r = 0
+
+# takes in a yt thumbnail url and returns true if there are more than 50 detected letters
+# the yt thumbnail (more like a bunch of color/activity in the yt thumbnail)
+
+count= 0
+
+def clickbaitImage(url):
+    global count
+    try:
+        hqres= url.split("/")
+        hqres[5] = "hqdefault.jpg"
+        hqres= "/".join(str(x) for x in hqres)
+        r = requests.get(hqres)
+        #print(hqres)
+    except requests.exceptions.RequestException as e:
+        return 0
     
-    result = np.array([], dtype = np.double)
-    avg_class0 = np.array([np.average(class0_arr)], dtype = np.double)
-    result = np.append(result, avg_class0)
-    avg_class1 = np.array([np.average(class1_arr)], dtype = np.double)
-    result = np.append(result, avg_class1)
-    avg_class2 = np.array([np.average(class2_arr)], dtype = np.double)
-    result = np.append(result, avg_class2)
-    avg_class3 = np.array([np.average(class3_arr)], dtype = np.double)
-    result = np.append(result, avg_class3)
-    return result
-    
+    img = Image.open(io.BytesIO(r.content))
+
+    custom_config = r'--oem 3 --psm 6'
+    text = pytesseract.image_to_string(img, config = custom_config)
+
+    print(count)
+    count= count+1
+
+    if len(re.findall(r'[a-zA-Z]', text)) > 50:
+        return 1
+    return 0
+
 df_US = csv_conversion("data/USvideos.csv")
-print(df_US)
+df_US.to_csv("data/USProcessed.csv")
 #print(averageClickbaitScore(df_US))
 #df_RU = csv_conversion("data/RUvideos.csv")
 #df_MX = csv_conversion("data/MXvideos.csv")
@@ -81,5 +111,5 @@ print(df_US)
 #df_GB = csv_conversion("data/GBvideos.csv")
 #df_FR = csv_conversion("data/FRvideos.csv")
 #df_DE = csv_conversion("data/DEvideos.csv")
-df_CA = csv_conversion("data/CAvideos.csv")
-
+#df_CA = csv_conversion("data/CAvideos.csv")
+#df_CA.to_csv("data/CAProcessed.csv")
